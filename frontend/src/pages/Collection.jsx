82a7f221bf83458ba6228fbe6ead4ea267943b92
irsCooper <spock187sheldon.gmail.com>
2024-04-08 23:26:06 +0400

    update

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,28 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Button, Form, Modal } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import {} from 'react-router-dom'
 
 export default function Collection(props) {
 
-    const [show, setShow] = useState(false);
-    const [_nft, set_Nft] = useState({name: '', desc: '', file: '', price: 0, amount: 0})
-
-    const file = (e) => {
-        const file = e.target.files[0]
-        set_Nft({..._nft, file: file.name})
-    }
-
-    const sNft = async (e) => {
-        e.preventDefault()
-
-        try {
-            const res = await props.contract.connect(props.signer).setNft(_nft.name, _nft.desc, _nft.file, _nft.price, _nft.amount, true) 
-            console.log(res)
-            alert('Подождите ответа блокчейна, результат скоро отобразится')
-        } catch(err) {
-            console.log(err)
-            alert(err)
-        }
-    }
+    const router = 
 
     return (
         <div>
@@ -36,24 +18,7 @@ export default function Collection(props) {
                     <p>Пока тут пусто, давайте добавим nft!</p>
             }
             
-            <Button onClick={() => setShow(true)}>создать nft</Button>
-
-            <Modal show={show} onHide={() => setShow(false)}>
-                <Modal.Body >
-                    <Form className="mb-3" onSubmit={sNft} style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}} >
-                        <h1 style={{textAlign: 'center'}}>Введите информацию о nft</h1>
-                        <Form.Group>
-                            <Form.Control value={_nft.name} onChange={e => set_Nft({..._nft, name: e.target.value})} style={{width: '280px'}} type="text" placeholder="введите название" required/>
-                            <Form.Control value={_nft.desc} onChange={e => set_Nft({..._nft, desc: e.target.value})} style={{width: '280px'}} type="text" placeholder="введите описание" required/>
-
-                            <Form.Control value={_nft.price} onChange={e => set_Nft({..._nft, price: e.target.value})} type="number" min={1} title="введите стоимость" required/>
-                            <Form.Control value={_nft.amount} onChange={e => set_Nft({..._nft, amount: e.target.value})} type="number" min={1} title="введите количество" required/>
-                            <input type="file" required onChange={file}/>
-                        </Form.Group>
-                        <Button style={{marginLeft: '10px', marginTop: '3%' ,width: '150px'}} variant="primary" type="submit">создать</Button>
-                    </Form> 
-                </Modal.Body>
-            </Modal>
+            
         </div>
     )
 }
